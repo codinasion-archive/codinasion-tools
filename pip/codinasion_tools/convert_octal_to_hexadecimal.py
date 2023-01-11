52811219ccaabb32ff2df29051803a30012c140e
Harsh Raj <harshraj8843@gmail.com>
2023-01-11 17:54:08 +0530

    pip update (#17)
    Co-authored-by: Baliram <heyome9a@gmail.com>

--- pip/codinasion_tools/convert_octal_to_hexadecimal.py
+++ pip/codinasion_tools/convert_octal_to_hexadecimal.py
@@ -5,5 +5,5 @@ from .convert_decimal_to_hexadecimal import ConvertDecimalToHexadecimal
 def ConvertOctalToHexadecimal(octal: int) -> str:
     """Convert an octal number to a hexadecimal number."""
     if octal == 0:
-        return 0
+        return "0"
     return ConvertDecimalToHexadecimal(ConvertOctalToDecimal(str(octal)))
--- pip/setup.py
+++ pip/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="codinasion-tools",
-    version="0.0.1",
+    version="0.0.2",
     author="Harsh Raj",
     author_email="harshraj8843@gmail.com",
     description="A collection of useful open source tools",
